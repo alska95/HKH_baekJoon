50eff78289da55cd02e21b29e888d45e2db88eef
Kyeong Ha Hwang <rhsalska55@naver.com>
2020-10-03 23:03:41 +0900

    201003
    none

--- baekJoon_15649.cpp
+++ baekJoon_15649.cpp
@@ -1,38 +1,45 @@
+/*
+직각삼각형 구별법
+에이 제곱 플러스 비 제곱은 씨
+
+가장 큰것을 씨로 둔다.
+나머지 두개를 제곱한다.
+*/
 
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-const int MAX = 8 + 1;
-
-int N, M;
-int arr[MAX];
-bool visited[MAX];
-
-void func(int cnt)
-{
-	if (cnt == M)
-	{
-		for (int i = 0; i < M; i++)
-			cout << arr[i] << " ";
-		cout << "\n";
-		return;
+void quicksort(int L, int R, int a[]) {
+	int left = L, right = R, pivot = a[(L + R) / 2];
+	if (L >= R) return;
+	while (left <= right) {
+		while (a[left] < pivot) left++;
+		while (a[right] > pivot) right--;
+		if (left <= right) {
+			swap(a[left], a[right]);
+			left++;
+			right--:
+		}
 	}
 
-	for (int i = 1; i <= N; i++)
-		if (!visited[i])
-		{
-			visited[i] = true;
-			arr[cnt] = i;
-			func(cnt + 1);
-			visited[i] = false;
-		}
+	quicksort(left, R, a);
+	quicksort(L, right, a);
 }
 
-int main(void)
-{
-	cin >> N >> M;
+int main() {
+	int input[3] = { 1,1,1 };
 
-	func(0);
-	return 0;
+	do{
+		for (int i = 0; i < 3; i++) {
+			cin >> input[i];
+		}
+
+		quicksort(0, 2, input);
+
+		if (input[0] * input[0] + input[1] * input[1] == input[2] * input[2])
+			cout << "right";
+		else
+			cout << "wrong";
+	} while (input[0] == 0 && input[1] == 0 && input[2] == 0)
 }
