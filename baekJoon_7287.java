191b120e5641c0cc3eb772bb381eeb132a57b623
alska95 <rhsalska55@naver.com>
2021-07-02 17:56:04 +0900

    7287

---
+++ baekJoon_7287.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("211");
+        System.out.println("alska95");
+
+    }
+}
