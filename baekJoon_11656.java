18cd03c8ad492d0553b0de4ebb8ceb73d902eb15
Kyeong Ha Hwang <rhsalska55@naver.com>
2021-07-10 23:00:23 +0900

    1966

--- baekJoon_11656.java
+++ baekJoon_11656.java
@@ -1,16 +1,30 @@
-import java.io.*;
+Import java.io.*;
 import java.util.*;
-public class Main{
+
+public class Main {
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String target = br.readLine();
-        ArrayList<String> result = new ArrayList<>();
-        for(int i = 1 ; i < target.length() ; i++){
-            result.add(target.substring(target.length()-i));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+
+        String[] strArray = new String[s.length()];
+        // 입력 string 크기만큼 배열 생성
+        for (int i = 0; i < s.length(); i++) {
+            strArray[i] = s.substring(i,s.length());
         }
-        result.sort(Comparator.naturalOrder());
-        for (String s : result) {
-            System.out.println(s);
+        // 하나씩 잘라서 넣고
+        Arrays.sort(strArray);
+        // 알파벳순 정렬
+        for(String str : strArray) {
+            bw.write(str);
+            bw.write('\n');
         }
+
+        bw.flush();
+
     }
+
 }
