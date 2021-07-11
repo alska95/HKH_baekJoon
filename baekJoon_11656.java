a4ab927ad2e3732d6ff9d7d68c30c8725e9f2510
alska95 <rhsalska55@naver.com>
2021-07-12 00:47:11 +0900

    20920_sortMap_BufferedWriter_java

--- baekJoon_11656.java
+++ baekJoon_11656.java
@@ -1,30 +1,22 @@
-Import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String s = br.readLine();
-
-        String[] strArray = new String[s.length()];
-        // 입력 string 크기만큼 배열 생성
-        for (int i = 0; i < s.length(); i++) {
-            strArray[i] = s.substring(i,s.length());
+        char [] targetString = br.readLine().toCharArray();
+        String [] suffix = new String[targetString.length];
+        for(int i = 0 ; i < targetString.length; i++){
+            suffix[i] = "";
+            for(int j = targetString.length-i-1 ; j < targetString.length ; j++){
+                suffix[i] += targetString[j];
+            }
         }
-        // 하나씩 잘라서 넣고
-        Arrays.sort(strArray);
-        // 알파벳순 정렬
-        for(String str : strArray) {
-            bw.write(str);
-            bw.write('\n');
+        Arrays.sort(suffix);
+        for (String s : suffix) {
+            System.out.println(s);
         }
-
-        bw.flush();
-
     }
-
 }
