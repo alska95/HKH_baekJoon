7e7d87472f554ed1ac755bebf78cfa637c64ca18
Kyeong Ha Hwang <rhsalska55@naver.com>
2021-08-09 00:03:30 +0900

    5557_dp

--- baekJoon_2609.java
+++ baekJoon_2609.java
@@ -5,22 +5,8 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int end = n;
-        if(m < n)
-            end = m;
-        int lcd = 1;
-        int gcd = m*n;
-        for(int i = end ; i > 0 ; i--){
-            if(n%i == 0 && m%i ==0){
-                lcd = i;
-                gcd /=i;
-                break;
-            }
-        }
-        bw.write(lcd +"\n" + gcd);
+        int n = Integer.parseInt(br.readLine());
+
         bw.close();
     }
 }
