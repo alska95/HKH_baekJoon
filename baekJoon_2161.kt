240ba3a3066aca3d4b1324ca248f313dada5c1de
alska95 <rhsalska55@naver.com>
2021-08-23 00:27:10 +0900

    2161

---
+++ baekJoon_2161.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main(){
+    var n : Int= readLine()!!.toInt()
+    var q : Queue<Int> = LinkedList()
+    for(i in 1..n){
+        q.add(i)
+    }
+    while(!q.isEmpty()){
+        print("${q.poll()} ")
+        if(q.isEmpty())
+            break
+        else
+            q.add(q.poll())
+    }
+}
