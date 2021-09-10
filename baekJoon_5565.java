b9fb4e823fa739b5bad7f55b03553edd03965e73
alska95 <rhsalska55@naver.com>
2021-09-10 10:18:52 +0900

    5565

---
+++ baekJoon_5565.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int sum = Integer.parseInt(br.readLine());
+        int result = 0;
+        for(int i = 0 ; i < 9 ; i++){
+            result+=Integer.parseInt(br.readLine());
+        }
+        bw.write(sum-result+"");
+        bw.close();
+    }
+}
