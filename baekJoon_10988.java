731410eea388928c4be864f7d76dca5370365dd4
alska95 <rhsalska55@naver.com>
2021-09-22 11:04:01 +0900

    10988

---
+++ baekJoon_10988.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String target = br.readLine();
+        int flag = 1;
+        for(int i = 0 ; i < target.length()/2+1 ; i++){
+            if(target.charAt(i) != target.charAt(target.length()-1-i)){
+                flag = 0;
+                break;
+            }
+        }
+        bw.write(flag + "");
+        bw.close();
+    }
+}
