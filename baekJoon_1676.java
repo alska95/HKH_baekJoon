4abe1efce6cdabbaec36f3ca4ff641d86c268f44
alska95 <rhsalska55@naver.com>
2021-09-24 06:13:16 +0900

    1676

---
+++ baekJoon_1676.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int two = 0;
+        int five = 0;
+        for(int i = 2 ; i <= n ; i*=2)
+            two+=n/i;
+        for(int i = 5 ; i <= n ; i*=5)
+            five+=n/i;
+        bw.write(Math.min(two,five)+"");
+        bw.close();
+    }
+}
