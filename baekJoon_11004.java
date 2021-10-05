563ded9cc85be3e60d40138a7f56b797e7c4727f
alska95 <rhsalska55@naver.com>
2021-10-05 22:40:46 +0900

    11004

---
+++ baekJoon_11004.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int board [] = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        st= new StringTokenizer(br.readLine() , " ");
+        for(int i = 0 ; i < n ; i++){
+            pq.add(Integer.parseInt(st.nextToken()));
+        }
+        for(int i = 0 ; i < k-1 ; i++){
+            pq.poll();
+        }
+        bw.write(pq.peek()+"");
+        bw.close();
+    }
+}
