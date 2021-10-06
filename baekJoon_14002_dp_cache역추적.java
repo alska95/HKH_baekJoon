cd960492402165e7f0d6551feb5c2d4b1b794a09
alska95 <rhsalska55@naver.com>
2021-10-07 07:51:24 +0900

    14002_dp_cache역추적

---
+++ baekJoon_14002_dp_cache역추적.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int board[] = new int[n];
+        int cache[] = new int[n];
+        int dp[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            board[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+        int max = 0;
+        int maxIter = 0;
+        for(int i = 0 ; i < n  ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(board[i] > board[j]){
+                    if(dp[i] < dp[j]+1){
+                        cache[i] = j;
+                        dp[i] = dp[j]+1;
+                    }
+                }
+            }
+            if(dp[i] > max){
+                max = dp[i];
+                maxIter = i;
+            }
+        }
+        ArrayList<Integer> resultArray = new ArrayList<>();
+        int index = maxIter;
+        resultArray.add(board[maxIter]);
+        for(int i = 0 ; i < max-1 ; i++){
+            resultArray.add(board[cache[index]]);
+            index = cache[index];
+        }
+        bw.write(max + "\n");
+        for(int i = resultArray.size()-1 ; i >=0 ; i--){
+            bw.write(resultArray.get(i)+" ");
+        }
+        bw.close();
+    }
+}
