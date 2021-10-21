e4a903806703fd6f5dcdcf44b50f92c948f05555
alska95 <rhsalska55@naver.com>
2021-10-22 07:12:10 +0900

    1302

---
+++ baekJoon_1302.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Map<String ,Integer> board = new HashMap<>();
+
+        for(int i = 0 ; i < N ; i++){
+            String target = br.readLine();
+            if(board.containsKey(target))
+                board.put(target ,board.get(target)+1);
+            else
+                board.put(target , 1);
+        }
+        ArrayList<String> maxBook = new ArrayList<>();
+        int max = 0;
+        for( String key : board.keySet()){
+            int keyValue = board.get(key);
+            if(keyValue > max){
+                max = keyValue;
+                maxBook.clear();
+                maxBook.add(key);
+            }else if(keyValue == max){
+                maxBook.add(key);
+            }
+        }
+        Collections.sort(maxBook);
+        bw.write(maxBook.get(0));
+        bw.close();
+    }
+}
