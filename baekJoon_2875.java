e75d1e224f743f46c5278d9710a44c6b4b1e72f9
alska95 <rhsalska55@naver.com>
2021-11-30 21:16:55 +0900

    2875

---
+++ baekJoon_2875.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.valueOf(st.nextToken());
+        int m = Integer.valueOf(st.nextToken());
+        int k = Integer.valueOf(st.nextToken());
+
+        int max = 0;
+        for(int i = 0; i <= k; i++) {
+            int j = k - i;
+            int nowN = n - j;
+            int nowM = m - i;
+            if(nowN < 0 || nowM < 0) {
+                continue;
+            }
+
+            if(nowN / 2 >= nowM) {
+                max = Math.max(max, nowM);
+            }
+            else {
+                max = Math.max(max, nowN / 2);
+            }
+        }
+        System.out.println(max);
+    }
+}
