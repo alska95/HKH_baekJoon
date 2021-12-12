3a087f47bd3fcdc73aac406eb69d45e6c03594f5
alska95 <rhsalska55@naver.com>
2021-12-12 15:47:20 +0900

    10156

---
+++ baekJoon_10156.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str1[] = br.readLine().split( " ");
+        int N = Integer.parseInt(str1[0]);
+        int K = Integer.parseInt(str1[1]);
+        int F = Integer.parseInt(str1[2]);
+        int R = Math.max(N * K - F, 0);
+        bw.write(String.valueOf(R));
+        bw.flush();
+    }
+}
