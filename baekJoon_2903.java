4f03ebb561cafa0848f37561269906c829f6ca79
alska95 <rhsalska55@naver.com>
2021-12-18 11:22:48 +0900

    2903

---
+++ baekJoon_2903.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println((long)Math.pow(1+Math.pow(2, new Scanner(System.in).nextInt()),2));
+    }
+}
