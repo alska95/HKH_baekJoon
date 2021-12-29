3fb391ce51f18a4de196034a8577e446099c8ec6
alska95 <rhsalska55@naver.com>
2021-12-29 22:09:58 +0900

    1668

---
+++ baekJoon_1668.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+        int count = 0;
+        int max = 0;
+        for (int i=0; i<num; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (i == 0) {
+                max = arr[i];
+                count++;
+                continue;
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+                count++;
+            }
+        }
+
+        bw.write(count+"\n");
+        count = 1;
+        max = arr[num-1];
+        for (int i=num-2; i>=0; i--) {
+            if (max < arr[i]) {
+                max = arr[i];
+                count++;
+            }
+        }
+        bw.write(count+"\n");
+        bw.close();
+    }
+}
