61f59c5ad62a318aff80e163fd788be72bf79515
alska95 <rhsalska55@naver.com>
2022-01-02 09:58:17 +0900

    3449

---
+++ baekJoon_3449.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < n ; i++){
+            String a = br.readLine();
+            String b = br.readLine();
+            int result = 0;
+            for(int j = 0 ; j < a.length() ; j++){
+                if(a.charAt(j) != b.charAt(j))
+                    result++;
+            }
+            sb.append("Hamming distance is ").append(result).append(".\n");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
