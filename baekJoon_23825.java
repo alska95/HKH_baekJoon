079cc1c3d2e17d664275ca0421bd57288b09a82d
alska95 <rhsalska55@naver.com>
2022-01-04 21:03:21 +0900

    23825

---
+++ baekJoon_23825.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] a){
+        StringTokenizer s = new StringTokenizer(new Scanner(System.in).nextLine(), " ");
+        System.out.print(Math.min(Integer.parseInt(s.nextToken()),Integer.parseInt(s.nextToken()))/2);
+    }
+}
