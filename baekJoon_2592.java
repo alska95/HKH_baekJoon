e4c7b9e95945531982b7b1c70ade9aa91a53a0f5
alska95 <rhsalska55@naver.com>
2022-01-13 21:38:17 +0900

    2592

---
+++ baekJoon_2592.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int max = 0;
+		int maxIter = 0;
+		int board[] = new int[1000];
+		double sum = 0;
+		for(int i = 0 ; i < 10 ; i++){
+			int target = Integer.parseInt(br.readLine());
+			sum+=target;
+			board[target] +=1;
+			if(board[target] > max){
+				maxIter = target;
+				max = board[target];
+			}
+		}
+		sum/=10;
+		System.out.println((int)Math.round(sum) +"\n"+maxIter);
+	}
+}
