0667e1cbbc25ffb98e3ea9b5ec0633028616b8a6
alska95 <rhsalska55@naver.com>
2022-01-16 14:37:45 +0900

    2857

---
+++ baekJoon_2857.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1 ; i < 6 ; i++){
+			if(br.readLine().contains("FBI"))
+				sb.append(i).append(" ");
+		}
+
+		if(sb.length() > 0)
+			System.out.println(sb);
+		else
+			System.out.println("HE GOT AWAY!");
+	}
+}
