52f22899acbc257d21c3da98a227bd788c9aa6af
alska95 <rhsalska55@naver.com>
2022-01-21 22:52:21 +0900

    2783

---
+++ baekJoon_2783.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Double> pq = new PriorityQueue<>();
+		pq.add(Double.parseDouble(st.nextToken())/Double.parseDouble(st.nextToken()));
+		for(int i = 1 ; i <= n ; i++){
+			st =new StringTokenizer(br.readLine(), " ");
+			pq.add(Double.parseDouble(st.nextToken())/Double.parseDouble(st.nextToken()));
+		}
+		bw.write(Math.round(pq.poll()*100000.0)/100.0+"");
+		bw.close();
+	}
+
+}
