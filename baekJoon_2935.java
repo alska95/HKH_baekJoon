dcbeeecb45de1fc9ab5eb9cfaed2c84ae8b2f90e
alska95 <rhsalska55@naver.com>
2022-02-08 23:10:07 +0900

    2935

---
+++ baekJoon_2935.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BigInteger a = new BigInteger(br.readLine());
+		String op = br.readLine();
+		BigInteger b = new BigInteger(br.readLine());
+
+		if(op.equals("+")) {
+			bw.write(a.add(b).toString());
+		}else{
+			bw.write(a.multiply(b).toString());
+		}
+		bw.close();
+	}
+}
