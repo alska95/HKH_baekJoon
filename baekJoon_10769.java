8c58ab39d738d0d37141c7733ad1143acfb92f7a
alska95 <rhsalska55@naver.com>
2022-03-18 09:40:20 +0900

    10769

---
+++ baekJoon_10769.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Pattern patternH = Pattern.compile(":-\\)");
+        Pattern patternU = Pattern.compile(":-\\(");
+        String target = br.readLine();
+        Matcher matcherH = patternH.matcher(target);
+        Matcher matcherU = patternU.matcher(target);
+        long happy = matcherH.results().count();
+        long unHappy = matcherU.results().count();
+
+        if(happy+unHappy == 0){
+            System.out.printf("none");
+        }else if(happy > unHappy){
+            System.out.printf("happy");
+        }else if(unHappy>happy){
+            System.out.printf("sad");
+        }else{
+            System.out.printf("unsure");
+        }
+    }
+}
