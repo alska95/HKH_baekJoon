f8c3332328046ab94143e4df7cf5b7f516b4754a
alska95 <rhsalska55@naver.com>
2022-05-09 23:12:25 +0900

    2789

---
+++ baekJoon_2789.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String target = br.readLine();
+        String filter = "CAMBRIDGE";
+        for(int i = 0 ; i< filter.length() ; i++){
+            target = target.replaceAll(String.valueOf(filter.charAt(i)), "");
+        }
+        bw.write(target);
+        bw.close();
+    }
+}
