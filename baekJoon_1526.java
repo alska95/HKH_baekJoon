3fee032fd914315f6d0fddda1c5e7d6e015b4347
alska95 <rhsalska55@naver.com>
2022-06-27 23:20:09 +0900

    2979

--- baekJoon_1526.java
+++ baekJoon_1526.java
@@ -1,29 +1,24 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String target = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        int cur = 0;
-        int flag = 0;
-        for(int i = target.length()-1 ; i >= 0; i--){
-            cur += target.charAt(i)-48;
-            if(flag == 1){
-                sb.insert(0,"7");
-            } else if(cur >= 7){
-                sb.insert(0,"7");
-                cur = (cur - 7) *10;
-            }else if(cur >= 4){
-                sb.insert(0,"4");
-                cur = (cur - 4) *10;
-            }else{
-                flag =1;
+        String N = br.readLine();
+        while(true) {
+            boolean check = true;
+            for(int i = 0; i < N.length(); i++) {
+                if(N.charAt(i) != '4' && N.charAt(i) != '7') {
+                    check = false;
+                    break;
+                }
             }
+            if(check == true) break;
+            else N = String.valueOf(Integer.parseInt(N) - 1);
         }
-        bw.write(sb.toString());
-        bw.close();
+        System.out.println(N);
     }
+
 }
