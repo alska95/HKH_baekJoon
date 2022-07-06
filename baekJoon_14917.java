8d358f6c49c4f5143bf18321bf56ab83fd227d6b
alska95 <rhsalska55@naver.com>
2022-07-06 21:41:18 +0900

    14917

---
+++ baekJoon_14917.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(a,b);
+        for(int i = 1 ; i <= min ; i++){
+            if( a % i == 0 && b % i == 0){
+                bw.write(i + " " + a / i + " " + b/i + "\n");
+            }
+        }
+        bw.close();
+    }
+}
