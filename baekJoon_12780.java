e7e8ddf8dfe1d115696be77a8c5c77c2065cc76c
alska95 <rhsalska55@naver.com>
2022-07-18 22:43:20 +0900

    12780

---
+++ baekJoon_12780.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String h = br.readLine();
+        String target = br.readLine();
+
+        Pattern pattern = Pattern.compile(target);
+        Matcher matcher = pattern.matcher(h);
+        int count = 0;
+        while(matcher.find()){
+            count++;
+        }
+        bw.write(count+"");
+        bw.close();
+    }
+}
