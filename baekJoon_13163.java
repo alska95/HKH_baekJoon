16d4c5f7baa6798fd30836dd71354961a6bfec65
alska95 <rhsalska55@naver.com>
2022-09-09 12:59:29 +0900

    7567

--- baekJoon_13163.java
+++ baekJoon_13163.java
@@ -5,18 +5,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < n ; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            st.nextToken();
-            sb.append("god");
-            while(st.hasMoreTokens()){
-                sb.append(st.nextToken());
-            }
-            sb.append("\n");
-        }
-        bw.write(sb.toString());
-        bw.close();
+        ㄴ
     }
 }
---
+++ baekJoon_7567.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String target = br.readLine();
+        int result = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (i != 0 && target.charAt(i - 1) == target.charAt(i)) {
+                result += 5;
+            } else {
+                result += 10;
+            }
+        }
+        bw.write(result+"");
+        bw.close();
+    }
+}
