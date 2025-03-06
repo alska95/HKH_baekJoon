c18ccd5a0d4e57b16572510697fdea9c7092d2c4
alska95 <rhsalska55@naver.com>
2025-03-07 08:59:07 +0900

    leetcode17

--- baekjoon/baekJoon_1924.java
+++ baekjoon/baekJoon_1924.java
@@ -27,7 +27,7 @@ public class Main {
 //        import java.io.InputStreamReader;
 //        import java.util.StringTokenizer;
 //
-//public class Main {
+//public class leetcode.Main {
 //    public static void main(String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        StringTokenizer st = new StringTokenizer(br.readLine() , " ");
