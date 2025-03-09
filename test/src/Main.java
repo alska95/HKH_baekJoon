21cee3896b722a18763dc586842ae463ba60eb28
alska95 <rhsalska55@naver.com>
2025-03-10 08:10:49 +0900

    leetcode7

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("args = " + ('1' - '0'));
+        System.out.println("args = " + Integer.parseInt("01"));
     }
 }
