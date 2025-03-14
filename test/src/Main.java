a47ece6c2f710d23e77f8dd9774aeaf989175797
alska95 <rhsalska55@naver.com>
2025-03-15 08:26:51 +0900

    leetcode189

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.length();
         System.out.println("args = " + ("01").substring(0, 2));
+        Arrays.copyOf()
     }
 }
