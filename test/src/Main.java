e3a30c5628d29f6cc7ac93f5bd90bcffabfb3559
alska95 <rhsalska55@naver.com>
2025-03-17 08:51:44 +0900

    leetcode206

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,10 +1,25 @@
 import java.util.Arrays;
 
 public class Main {
+    static class Node {
+        Node next;
+        Node(Node next) {
+            this.next = next;
+        }
+        Node(){}
+    }
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.length();
-        System.out.println("args = " + ("01").substring(0, 2));
-        Arrays.copyOf()
+        Node node = new Node();
+        Node second = new Node();
+
+        node.next = second;
+        Node temp = node.next;
+        node.next = null;
+        Node tempNode = node;
+        node = second;
+        System.out.println("temp = " + temp);
+        System.out.println("first = " + node);
+        System.out.println("temp2 = " + tempNode);
+        System.out.println("second = " + second);
     }
 }
