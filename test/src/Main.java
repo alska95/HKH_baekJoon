ebbc2812fbaad33cc983175ebd5498ecfd08e61b
alska95 <rhsalska55@naver.com>
2025-03-20 08:13:09 +0900

    leetcode51

--- test/src/Main.java
+++ test/src/Main.java
@@ -24,6 +24,7 @@ public class Main {
         System.out.println("temp2 = " + tempNode);
         System.out.println("second = " + second);
         List<Integer> startList = new ArrayList<>();
+        startList.remove
         int[] array = startList.stream().sorted().mapToInt(a -> a).toArray();
 
     }
